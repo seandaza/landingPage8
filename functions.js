a5e7b94b858151269b641b1b8a3d87f5248508d9
Sean Daza <60199238+seandaza@users.noreply.github.com>
2022-05-02 12:09:36 -0500

    first commit

---
+++ functions.js
@@ -0,0 +1,5 @@
+
+document.getElementById("icon-menu").addEventListener("click", function(){
+    document.getElementById("move-content").classList.toggle("move-container-all");
+    document.getElementById("show-menu").classList.toggle("show-lateral");
+});
